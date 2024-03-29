34891f12de8a00baf17cbd8444f455d6de038551
Warren Bevilacqua <bevilacqua.warren@gmail.com>
2024-03-29 10:08:39 +0100

    feat(header): add a header view for the website

---
+++ resources/views/header.blade.php
@@ -0,0 +1,40 @@
+<?php
+/** Header view
+ *  -------
+ *  @file
+ *  @brief Header
+ */
+?>
+
+@if(session('user_id'))
+    @if(session('type') == 1)
+        <h2>Bonjour administrateur {{ session('name') }}, bienvenue dans l'interface de modification :</h2>
+    @else
+        <h2>Bonjour client {{ session('name') }}, bienvenue dans votre boutique :</h2>
+    @endif
+@endif
+
+<ul id="menu">
+	<li style="background-color:#3febdc">
+		<a href="index.php?uc=accueil"> Accueil </a>
+	</li>
+	<li style="background-color:#3febdc">
+		<a href="index.php?uc=voirProduits&action=voirCategories"> Voir le catalogue de fleurs </a>
+	</li>
+
+	@if(session('type') && session('type') == 2)
+		<li style="background-color:#3febdc" ><a href="index.php?uc=gererPanier&action=voirPanier"> Voir mon panier </a></li> 
+	@elseif(session('type') && session('type') == 1)
+		<li style="background-color:#3febdc" ><a href="index.php?uc=gestionProduits&action=Ajouter"> Ajouter un Produit </a></li>
+	@endif
+
+	@if(!session('name'))
+		<li style="float:right; background-color:#3febdc">
+			<a href="{{ route('connection.form') }}"> Se connecter </a>
+		</li>
+	@else
+		<li style="float:right; background-color:#3febdc">
+			<a href="{{ route('connection.deconnection') }}"> Se déconnecter </a>
+		</li>
+	@endif
+</ul>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('welcome')->with('header', view('header'));
+})->name('welcome');
